14c1438427f0b4b91c44319bfedbbb6f4d26053e
umarovmuhammadali9997-hash <umarovmuhammadali9997@gmail.com>
2026-05-26 13:20:10 +0500

    Add files via upload

---
+++ config (2).py
@@ -0,0 +1,61 @@
+# ========================
+# SOZLAMALAR
+# ========================
+
+BOT_TOKEN = "8632295415:AAHX0PjG9BRyrLWObEy8y8-REolYgZa77FU"
+
+SUPER_ADMINS = [
+    800908885,  # Muhammadali Umarov
+    977120595,  # Asadulloh Rustamov
+]
+
+SECTIONS = {
+    "atf": "⚡ ATF",
+    "dnk": "🧬 DNK",
+    "gametogenez": "🔄 Gametogenez",
+    "genetika": "🧪 Genetika",
+    "energetik": "🔋 Energetik almashinuv",
+    "nazariy_dars": "📖 Nazariy mavzular darsi",
+    "mavzu_testlar": "📝 Mavzular yuzasidan testlar",
+    "test_tahlil": "🔍 Nazariy test tahlillari",
+    "teacher_photo": "👨‍🏫 Muhammadali rasmi",
+    "teacher2_photo": "👨‍🏫 Asadulloh rasmi",
+    "sert_ms": "📚 M.S Kitoblar yechimi",
+    "sert_atf": "⚡ ATF murakkab masalalar",
+    "sert_dnk": "🧬 DNK murakkab masalalar",
+    "sert_gamet": "🔄 Gametogenez murakkab masalalar",
+    "sert_genetika": "🧪 Genetika murakkab masalalar",
+    "sert_xardi": "🌍 Xardi-Vaynberg masalalari",
+    "sert_qosh": "🔀 Qo'sh krosingover masalalari",
+    "sert_krosing": "🔁 Krosingover murakkab masalalari",
+    "dtm_2019": "📄 DTM 2019",
+    "dtm_2020": "📄 DTM 2020",
+    "dtm_2021": "📄 DTM 2021",
+    "dtm_2022": "📄 DTM 2022",
+    "dtm_2023": "📄 DTM 2023",
+    "dtm_2024": "📄 DTM 2024",
+    "dtm_botanika": "🌿 Botanika",
+    "dtm_zoologiya": "🐾 Zoologiya",
+    "dtm_sitologiya": "🧬 Sitologiya",
+    "dtm_genetika": "🧫 Genetika DTM",
+    "dtm_mikrobio": "🦠 Mikrobiologiya",
+    "dtm_ekologiya": "🌱 Ekologiya",
+}
+
+SOCIAL_LINKS = {
+    "telegram": "https://t.me/biolog_UMM",
+    "instagram": "https://instagram.com/biolog_Muhammadali_master_sefr",
+    "youtube": "https://youtube.com/@muhammadaliumarov4934",
+}
+
+UMM_PER_REFERRAL = 2
+UMM_PER_PREMIUM_REF = 50
+UMM_FOR_PREMIUM = 100
+
+PREMIUM_PRICES = {
+    1: 50000,
+    3: 120000,
+    6: 200000,
+}
+
+BOT_USERNAME = "umm_biologiya_bot"
